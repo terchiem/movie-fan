5baa5fb4804f119cee45f13fda75fef51f88260c
Terry Chiem <terrychiem@gmail.com>
2020-11-24 23:44:59 -0800

    Add React router and placeholder routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header/Header';
@@ -5,8 +7,16 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
 
+        <Route exact path="/">
+          <p>I'm the root page</p>
+        </Route>
+        <Route path="/:id">
+          <p>I'm a movie details page</p>
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
