aa3163d8d2f6b9776545c47d6e1ba4b8e479ec81
Terry Chiem <terrychiem@gmail.com>
2020-11-29 13:29:49 -0800

    Add color theme to all styling

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,9 @@ import './App.css';
 import Header from './components/Header/Header';
 import Routes from './components/Routes/Routes';
 
-const TEST_MOVIES = [
-  { id: 1, title: 'movie 1', year: 2000 },
-  { id: 2, title: 'movie 2', year: 2010 },
-  { id: 3, title: 'movie 3', year: 'N/A' },
-]
-
 function App() {
   const history = useHistory();
-  const [movies, setMovies] = useState(TEST_MOVIES);
+  const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const search = async (searchTerm) => {
