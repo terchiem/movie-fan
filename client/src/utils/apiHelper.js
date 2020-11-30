ee5d77e9929f5f42a809334e28871697db646e08
Terry Chiem <terrychiem@gmail.com>
2020-11-30 12:17:02 -0800

    Update api url

--- client/src/utils/apiHelper.js
+++ client/src/utils/apiHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3005/';
+const BASE_URL = 'https://movie-fan-app.herokuapp.com/';
 const SEARCH_URL = BASE_URL + 'omdb/search';
 const GET_URL = BASE_URL + 'omdb/movie';
 
